2041f54c1b4a0f750e9e726e8bde999097ad3598
johanicp123 <johanicp123@gmail.com>
2022-05-02 21:49:57 -0500

    javascript
    subo la parte javascript de practica de indexedDB

--- js/app.js
+++ js/app.js
@@ -2,13 +2,38 @@
 
     let DB;
 
+    const listadoClientes = document.querySelector('#listado-clientes');
+
     document.addEventListener('DOMContentLoaded',()=>{
         crearDB();
 
         if(window.indexedDB.open('crm',1)){
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click',eliminarRegistro);
     });
+
+    function eliminarRegistro(e){
+        if(e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+            const confirmar = confirm('Deseas eliminar este cliente');
+            if(confirmar){
+                const transaction = DB.transaction(['crm'],'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function(){
+                    console.log('eliminado.')
+                    e.target.parentElement.parentElement.remove();
+                }
+                transaction.onerror = function(){
+                    console.log('Hubo un error');
+                }
+            }
+        };
+    }
 
     function crearDB(){
         const crearDB = window.indexedDB.open('crm',1);
@@ -45,7 +70,35 @@
         abrirConexion.onsuccess = function (){
             DB = abrirConexion.result;
             const objectStore = DB.transaction('crm').objectStore('crm');
-            
+            objectStore.openCursor().onsuccess = function(e){
+                const cursor = e.target.result;
+
+                if(cursor){
+                    const {nombre,empresa,email,telefono,id} = cursor.value;
+                   
+                    listadoClientes.innerHTML += `
+                    <tr>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <p class="text-sm lead-5 font-medium text-gray-700 text-lg font-bold"> ${ nombre } </p>
+                        <p class="texto-sm líder-10 texto-gris-700"> ${ email } </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                        <p class="text-gray-700"> ${ telefono } </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 lead-5 text-gray-700">    
+                        <p class="text-gray-600"> ${ empresa } </p>
+                    </td>
+                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm lead-5">
+                        <a href="editar-cliente.html?id= ${ id } " class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                        <a href="#" data-cliente=" ${ id } " class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                    </td>
+                    </tr>
+            ` ;
+                    cursor.continue();
+                }else{
+                    console.log('no hay mas registros');
+                }
+            }
         };
     }
 })();
