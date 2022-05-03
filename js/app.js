de774c09f931599c67ab06b2099e61cf7fa6b90c
johanicp123 <johanicp123@gmail.com>
2022-05-02 19:36:12 -0500

    actualizacion

---
+++ js/app.js
@@ -0,0 +1,51 @@
+(function(){
+
+    let DB;
+
+    document.addEventListener('DOMContentLoaded',()=>{
+        crearDB();
+
+        if(window.indexedDB.open('crm',1)){
+            obtenerClientes();
+        }
+    });
+
+    function crearDB(){
+        const crearDB = window.indexedDB.open('crm',1);
+
+        crearDB.onerror = function (){
+            console.log('Hubo un error');
+        };
+
+        crearDB.onsuccess = function (){
+            DB = crearDB.result;
+        };
+
+        crearDB.onupgradeneeded = function (e){
+            const db = e.target.result;
+
+            const objectStore = db.createObjectStore('crm', {keyPath:'id', autoIncrement:true});
+
+            objectStore.createIndex('nombre','nombre',{unique:false});
+            objectStore.createIndex('email','email',{unique:true});
+            objectStore.createIndex('telefono','telefono',{unique:false});
+            objectStore.createIndex('empresa','empresa',{unique:false});
+            objectStore.createIndex('id','id',{unique:true});
+            
+            console.log('DB lista y creada');
+        }
+    }
+
+    function obtenerClientes(){
+        const abrirConexion = window.indexedDB.open('crm',1);
+
+        abrirConexion.onerror = function (){
+            console.log('Hubo un error');
+        };
+        abrirConexion.onsuccess = function (){
+            DB = abrirConexion.result;
+            const objectStore = DB.transaction('crm').objectStore('crm');
+            
+        };
+    }
+})();
